fb54a5378924613a3fdaf16203dd8b2a28fd7051
jovepsala <jovepsal@cs.helsinki.fi>
2016-10-11 14:22:15 +0300

    RPC and services

--- application/eventchannel.js
+++ application/eventchannel.js
@@ -1,32 +1,62 @@
 #!/usr/bin/env node
 /**
- * app-lightcontrol, 6.10.2016 Spaceify Oy
+ * EventChannel, 6.10.2016 Spaceify Oy
  *
  * @class EventChannel
  */
 
-var spaceify = require("/api/spaceifyapplication.js");
+//var spaceify = require("/api/spaceifyapplication.js");
 
 function EventChannel()
 {
 var self = this;
 
+var eventChannelService;
+
 var listeners = new Array();
-var listenersByName = new Object();
+var listenersByEventName = new Object();
 
 // EXPOSED JSON-RPC METHODS -- -- -- -- -- -- -- -- -- -- //
+
+
+self.onClientDisconnected = function(connectionId)
+	{
+	for(var name in listenersByEventName)										// Go through all the listener by event name
+		{
+		for(var i = 0; i < listenersByEventName[name].length; i++)				// and try to find the listeners with the connectionId
+			{
+			if(listenersByEventName[name][i].connectionId == connectionId)
+				listenersByEventName[name].splice(i, 1);
+			}
+
+		if(listenersByEventName[name].length == 0)								// Delete empty listener objects
+			delete listenersByEventName[name];
+		}
+
+	for(var i = 0; i < listeners.length; i++)									// Delete the listeners from here too						
+		{
+		if(listeners[i].connectionId == connectionId)
+			listeners.splice(i, 1);
+		}
+	}
 
 // Adds and EventListener that gets triggered every time an event that matches the eventFilter
 // is generated. When the EventListener is triggered, the RPC function with callbackName
 // should be called on the application that added this event listener.
 
-self.addEventListener = function(eventFilter, callbackName, callObj)
+self.addEventListener = function(eventFilter, callbackName)
 	{
-	var listener = {filter: eventFilter, connectionId: callObj.callerId, callbackName};
-	listeners.push(listener);
-	listenersByName = [eventFilter.name] = [listener];
-	};
+	var listener = {filter: eventFilter, connectionId: arguments[arguments.length - 1].connectionId, callbackName: callbackName};
 
+	// ToDo: check duplicates?
+
+	listeners.push(listener);
+
+	// listenersByEventName = [eventFilter.name] = [listener]; ???
+	if(!listenersByEventName.hasOwnProperty(eventFilter.name))					// Multiple clients can listen to the same event
+		listenersByEventName[eventFilter.name] = [];
+	listenersByEventName[eventFilter.name].push(listener);
+	};
 
 // Generates (= emits) event on this eventChannel. Triggers all eventListeners, the filter of
 // which matches the eventObject
@@ -38,26 +68,28 @@ self.generateEvent = function(eventObject)
 	// Implement first exact matching of the event name,
 	// More complex filters will be added later!
 
-	if (listenersByName.hasOwnProperty(eventObject.name))
-	 	{
-		var lis = listenersByName[eventObject.name];
+	if (listenersByEventName.hasOwnProperty(eventObject.name))
+		{
+		var lis = listenersByEventName[eventObject.name];
 
-		for (var i = 0; i < lis.length; i++ )
+		for (var i = 0; i < lis.length; i++)
 			{
-			communicator.callRpc(lis[i].callbackName, [eventObject], lis[i].connectionId);		//!!!!! TODO make this really work!
+			// Clients connect to our provided service. Clients can be called individually by using their connectionId.
+			eventChannelService.callRpc(lis[i].callbackName, [eventObject], null, null, lis[i].connectionId);
 			}
 		}
 	};
 
-
-	// Remember to IMPLEMENT start AND fail METHODS IN YOUR APPLICATION!!! -- --  -- -- //
+	// Remember to IMPLEMENT start AND fail METHODS IN YOUR APPLICATION!!! -- -- -- -- //
 self.start = function()
 	{
 	// PROVIDED - CONNECTIONS FROM CLIENTS TO US
-	var service = spaceify.getProvidedService("spaceify.org/services/eventchannel");
+	eventChannelService = spaceify.getProvidedService("spaceify.org/services/eventchannel");
 
-	service.exposeRpcMethod("addEventListener", self, self.addEventListener);
-	service.exposeRpcMethod("generateEvent", self, self.generateEvent);
+	eventChannelService.exposeRpcMethod("addEventListener", self, self.addEventListener);
+	eventChannelService.exposeRpcMethod("generateEvent", self, self.generateEvent);
+
+	eventChannelService.setDisconnectionListener(onClientDisconnected);
 	};
 
 self.fail = function()
